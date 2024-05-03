17665e88feee4c2960c09ff30389fe2a49ca5da7
Daniel Martínez Duque <danielopq@gmail.com>
2024-05-04 00:48:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <div id="title">
+          <h1>TODO</h1>
+          <button>luna</button>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App;
