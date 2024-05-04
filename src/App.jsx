9ce111517b72b1098899f6eab09ce753d6469f68
Daniel Martínez Duque <danielopq@gmail.com>
2024-05-04 12:52:15 +0100

    main-container and menu created

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,36 @@ function App() {
           <button id="changeBg-bt"></button>
         </div>
         <form>
-          <div id="newTodo" className="todo">
+          <div id="newTodo" className="todo single-todo">
             <button className='state-bt unFinished-bt'></button>
             <input id="createTodo" type="text" placeholder='Create a new todo...'></input>
           </div>
         </form>
       </header>
       <main>
-        <section id="todoList">
-          <article id="tarea01" className="todo top-todo">
-            <button className='state-bt finished-bt'></button>
-            <p className='todoText completed'>Complete online JavaScript course</p>
-            <button className='delete-bt'></button>
-          </article>
-          <article id="tarea02" className="todo mid-todo">
-            <button className='state-bt unFinished-bt'></button>
-            <p className='todoText unCompleted'>Jog around the park 3x</p>
-            <button className='delete-bt'></button>
-          </article>
-          <div id="info-bar" className='todo bottom-todo'>
-            <p id="info" name="info">5 items left</p>
-            <button className="clear-bt">Clear Completed</button>
-          </div>
-        </section>
+        <div id="main-container">
+          <section id="todoList">
+            <article id="tarea01" className="todo top-todo">
+              <button className='state-bt finished-bt'></button>
+              <p className='todoText completed'>Complete online JavaScript course</p>
+              <button className='delete-bt'></button>
+            </article>
+            <article id="tarea02" className="todo mid-todo">
+              <button className='state-bt unFinished-bt'></button>
+              <p className='todoText unCompleted'>Jog around the park 3x</p>
+              <button className='delete-bt'></button>
+            </article>
+            <div id="info-bar" className='todo bottom-todo'>
+              <p id="info" name="info">5 items left</p>
+              <button className="clear-bt">Clear Completed</button>
+            </div>
+          </section>
+          <menu className='todo single-todo'></menu>
+        </div>
+
+
       </main>
+
     </>
   )
 }
