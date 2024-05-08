424cbe46a646742b93b612c223abc95c209f1ea8
Daniel Martínez Duque <danielopq@gmail.com>
2024-05-08 15:58:17 +0100

    info dark mode done

--- src/assets/components/TodoCompleted.jsx
+++ src/assets/components/TodoCompleted.jsx
@@ -1,7 +1,7 @@
 const TodoCompleted = ({uncompletedTodos,clearCompleted}) => {
     return (
         <div id="info-bar" className='container bg-light-mode'>
-            <p id="info" name="info">{uncompletedTodos} items left</p>
+            <p id="info" className="info-light-mode" name="info">{uncompletedTodos} items left</p>
             <button className="clear-bt bt-light-mode" onClick={clearCompleted}>Clear Completed</button>
         </div>
     )
