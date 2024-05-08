c508d4848d4b544edf09ca9bd0b35000fb7bcbfb
Daniel Martínez Duque <danielopq@gmail.com>
2024-05-08 15:54:03 +0100

    creal button states done

--- src/assets/components/TodoCompleted.jsx
+++ src/assets/components/TodoCompleted.jsx
@@ -2,7 +2,7 @@ const TodoCompleted = ({uncompletedTodos,clearCompleted}) => {
     return (
         <div id="info-bar" className='container bg-light-mode'>
             <p id="info" name="info">{uncompletedTodos} items left</p>
-            <button className="clear-bt" onClick={clearCompleted}>Clear Completed</button>
+            <button className="clear-bt bt-light-mode" onClick={clearCompleted}>Clear Completed</button>
         </div>
     )
 }
