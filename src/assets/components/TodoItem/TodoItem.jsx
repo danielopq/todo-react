ee14d7feb76ea60b4e01ab6e2f6b91c066ff9f91
Daniel Martinez Duque <danielopq@gmail.com>
2024-11-21 19:17:26 +0000

    TodoItem.jsx - cleaning code almost finished

--- src/assets/components/TodoItem/TodoItem.jsx
+++ src/assets/components/TodoItem/TodoItem.jsx
@@ -1,32 +1,34 @@
+import './todoItem.css';
+
 const TodoItem = ({todo,removeTodo,updateTodo,mode}) => {
     const{id,title,completed} = todo;
 
     const articleClass = ()=>{
-        let artClass = "container bg-light-mode task-light-mode";
+        let artClass = "todo bg-light-mode task-light-mode";
         if(mode === "dark"){artClass = artClass.replace(/light/g,"dark");}
         return artClass;
     }
 
     const buttonClass = ()=>{
         let btClass = "";
-        completed ? btClass = 'state-bt finished-bt bg-light-mode' : btClass = 'state-bt unFinished-bt-light-mode bg-light-mode';
+        completed ? btClass = 'state-bt finished-bt bg-light-mode' : btClass = 'state-bt unFinished-bt-light bg-light-mode';
         mode === "dark" && (btClass = btClass.replace(/light/g,"dark"));
         return btClass;
     }
-
+    
     const paragClass = ()=>{
         let pClass = "";
-        completed ? pClass = 'todoText completed-light-mode' : pClass = 'todoText unCompleted-light-mode'
+        completed ? pClass = 'todoText completedTask-light' : pClass = 'todoText unCompletedTask-light'
         mode === "dark" && (pClass = pClass.replace(/light/g,"dark"));
         return pClass;
     }
 
     return (
-        <article id={id} className={articleClass()}>
+        <div id={id} className={articleClass()}>
             <button className={buttonClass()} onClick={()=>updateTodo(id)}></button>
             <p className={paragClass()}>{title}</p>
             <button className='delete-bt' onClick={()=>removeTodo(id)}></button>
-        </article>
+        </div>
     )
 }
 
