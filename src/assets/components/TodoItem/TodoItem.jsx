207726ff2047a09228804e4fa1f07def4919b680
Daniel Martinez Duque <danielopq@gmail.com>
2024-11-24 00:39:03 +0000

    TodoItem.jsx

--- src/assets/components/TodoItem/TodoItem.jsx
+++ src/assets/components/TodoItem/TodoItem.jsx
@@ -1,37 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import './todoItem.css';
 
 const TodoItem = ({ todo, removeTodo, updateTodo, mode }) => {
     const { id, title, completed } = todo;
+    const refTodo = useRef(null);
+    const refStateButton = useRef(null);
+    const refTodoTitle = useRef(null);
 
-    /**
-     * Get the CSS class for the state button.
-     * Dynamically updates the class based on the completion status and mode (light or dark).
-     * @returns {string} - The CSS class for the state button.
-     */
-    const buttonStateClass = () => {
-        let btClass = "";
-        completed ? btClass = 'finished-bt' : btClass = 'unFinished-bt-light';
-        mode === "dark" && (btClass = btClass.replace(/light/g, "dark"));
-        return btClass;
-    }
-
-    /**
-     * Get the CSS class for the todo title.
-     * Dynamically updates the class based on the completion status and mode (light or dark).
-     * @returns {string} - The CSS class for the todo title.
-     */
-    const titleStateClass = () => {
-        let pClass = "";
-        completed ? pClass = 'completedTask-light' : pClass = 'unCompletedTask-light'
-        mode === "dark" && (pClass = pClass.replace(/light/g, "dark"));
-        return pClass;
-    }
+    useEffect(()=>{
+        if(refTodo.current && refStateButton.current && refTodoTitle.current){
+            refTodo.current.className =`todo task-${mode}`;
+            if(completed){
+                refStateButton.current.className = 'finished-bt';
+                refTodoTitle.current.className = `completedTask-${mode}`;
+            }else{
+                refStateButton.current.className = `unFinished-bt-${mode}`;
+                refTodoTitle.current.className = `unCompletedTask-${mode}`;
+            }
+        }
+    },[mode,completed])
 
     return (
-        <div id={id} className={ mode === 'dark' ? 'todo task-dark' : 'todo task-light'} style={{ backgroundColor: mode === 'dark' ? '#25273D' : 'white' }}>
-            <button aria-label='Update todo' className={buttonStateClass()} onClick={() => updateTodo(id)}></button>
-            <p className={titleStateClass()}>{title}</p>
+        <div id={id} ref={refTodo} style={{ backgroundColor: mode === 'dark' ? '#25273D' : 'white' }}>
+            <button ref={refStateButton} aria-label='Update todo' onClick={() => updateTodo(id)}></button>
+            <p ref={refTodoTitle}>{title}</p>
             <button aria-label='Delete todo' className='delete-bt' onClick={() => removeTodo(id)}></button>
         </div>
     )
