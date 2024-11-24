b4f39ab54fb32be63b436f8138ca63fd689336c3
Daniel Martinez Duque <danielopq@gmail.com>
2024-11-24 14:59:36 +0000

    TodoItem.jsx - comments added, grab and grabbing cursor state fixed

--- src/assets/components/TodoItem/TodoItem.jsx
+++ src/assets/components/TodoItem/TodoItem.jsx
@@ -1,24 +1,42 @@
 import { useEffect, useRef } from 'react';
 import './todoItem.css';
 
+/**
+ * Renders a single todo item in the list.
+ * Displays the todo's title, a button to toggle its completion state, 
+ * and a button to delete it from the todo list. 
+ * The appearance of the todo item changes dynamically based on its state 
+ * (completed or not) and the current skin mode (dark or light).
+ * 
+ * @param {Object} props - Component props.
+ * @param {Object} props.todo - Todo object containing the item's details.
+ * @param {string} props.todo.id - Unique identifier for the todo item.
+ * @param {string} props.todo.title - Title or description of the todo item.
+ * @param {boolean} props.todo.completed - Indicates if the todo is completed.
+ * @param {function} props.removeTodo - Function to delete a specific todo from the list.
+ * @param {function} props.updateTodo - Function to toggle the todo's completion state.
+ * @param {string} props.mode - Current skin mode ('dark' or 'light'). Defaults to 'dark'.
+ * @returns {JSX.Element} The TodoItem component.
+ */
 const TodoItem = ({ todo, removeTodo, updateTodo, mode }) => {
     const { id, title, completed } = todo;
     const refTodo = useRef(null);
     const refStateButton = useRef(null);
     const refTodoTitle = useRef(null);
 
-    useEffect(()=>{
-        if(refTodo.current && refStateButton.current && refTodoTitle.current){
-            refTodo.current.className =`todo task-${mode}`;
-            if(completed){
+    useEffect(() => {
+        // Updates the styles and classes of the todo item based on its state and the current mode.
+        if (refTodo.current && refStateButton.current && refTodoTitle.current) {
+            refTodo.current.className = `todo task-${mode}`;
+            if (completed) {
                 refStateButton.current.className = 'finished-bt';
-                refTodoTitle.current.className = `completedTask-${mode}`;
-            }else{
+                refTodoTitle.current.className = `title completedTask-${mode}`;
+            } else {
                 refStateButton.current.className = `unFinished-bt-${mode}`;
-                refTodoTitle.current.className = `unCompletedTask-${mode}`;
+                refTodoTitle.current.className = `title unCompletedTask-${mode}`;
             }
         }
-    },[mode,completed])
+    }, [mode, completed]);
 
     return (
         <div id={id} ref={refTodo} style={{ backgroundColor: mode === 'dark' ? '#25273D' : 'white' }} draggable="true">
