94afc07a19471f2be0e7d1c9fec4bceddd3a3fa9
Daniel Martinez Duque <danielopq@gmail.com>
2024-11-24 12:26:11 +0000

    Footer.jsx - comments added

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
-import './footer.css'
-const Footer = () =>{
-    return(
+import './footer.css';
+
+/**
+ * Footer component for the website.
+ * Displays a message indicating that the todo list items can be reordered using drag-and-drop.
+ * 
+ * @returns {JSX.Element} The Footer component.
+ */
+const Footer = () => {
+    return (
         <footer>
             <p>Drag and drop to reorder list</p>
         </footer>
-    )
-}
+    );
+};
+
 export default Footer;
